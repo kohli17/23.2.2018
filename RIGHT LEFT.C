cf38354d342c6bdc903e27c288d97174450615a4
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-23 15:37:27 +0530

    Create RIGHT LEFT.C
    print the range of a number

---
+++ RIGHT LEFT.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int a,b,c;
+    printf("Enterb the number\n");
+    scanf("%d",&a);
+    printf("Enter the range");
+    scanf("%d %d",&b,&c);
+    if(a>b&&a<c)
+    printf("Yes");
+    else
+    printf("No");
+}
+
